235d19859b1db3429c60274bd1940291ab129eb9
simon267 <top.spin@gmx.de>
2026-02-28 16:09:09 +0100

    Änderungen an der urls.py, damit nun auch Follower_URL verfügabr sind

--- kompy/constants/urls.py
+++ kompy/constants/urls.py
@@ -6,3 +6,6 @@ class KomootUrl:
     LIST_TOURS_URL: Final[str] = 'https://api.komoot.de/v007/users/{user_identifier}/tours/'
     TOUR_URL: Final[str] = 'https://api.komoot.de/v007/tours/{tour_identifier}'
     UPLOAD_TOUR_URL: Final[str] = 'https://api.komoot.de/v007/tours/?data_type={object_type}'
+    LIST_RELATION_URL: Final[str] = 'https://api.komoot.de/v007/users/{user_identifier}/relations/{relation_identifier}/'
+    LIST_FOLLOWER_URL: Final[str] = 'https://komoot.com/user/{user_identifier}/followers/'
+    PROFILE_URL: Final[str] = 'https://api.komoot.de/v007/users/{user_identifier}/'
